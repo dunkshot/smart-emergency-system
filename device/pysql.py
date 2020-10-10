cf20b27a19fbdb29523bae467f37645499e091b3
kcloud721 <kcloud721@naver.com>
2020-10-10 18:51:17 +0900

    update pysql.py syntax

--- device/pysql.py
+++ device/pysql.py
@@ -16,31 +16,31 @@ for line in lines:
     print (rr)
     print (rr[0])
     print (rr[1])
-
+caseID = case00001
 #i=rr.index('맥박')  #'맥박'검색 > 배열 번호 i에 저장
 
 if '혈압' in rr:
-    cursor.execute("UPDATE state SET pressure = %s WHERE caseID = 'case00001' ;", rr[rr.index('혈압')+1])
+    cursor.execute("UPDATE state SET pressure = %s WHERE caseID = %s ;", rr[rr.index('혈압')+1], caseID)
     print("UPDATE 혈압")
 
 if '맥박' in rr:
-    cursor.execute("UPDATE state SET heartrate = %s WHERE caseID = 'case00001' ;", rr[rr.index('맥박')+1])
+    cursor.execute("UPDATE state SET heartrate = %s WHERE caseID = %s ;", rr[rr.index('맥박')+1], caseID)
     print("UPDATE 맥박")
 
 if '체온' in rr:
-    cursor.execute("UPDATE state SET temperature = %s WHERE caseID = 'case00001' ;", rr[rr.index('체온')+1])
+    cursor.execute("UPDATE state SET temperature = %s WHERE caseID = %s ;", rr[rr.index('체온')+1], caseID)
     print("UPDATE 체온")
 
 if '이름' in rr:
-    cursor.execute("UPDATE patient SET P_name = %s WHERE caseID = 'case00001' ;", rr[rr.index('이름')+1])
+    cursor.execute("UPDATE patient SET P_name = %s WHERE caseID = %s ;", rr[rr.index('이름')+1], caseID)
     print("UPDATE 이름")
 
 if '증상' in rr:
-    cursor.execute("UPDATE state SET symptom = %s WHERE caseID = 'case00001' ;", rr[rr.index('증상')+1])
+    cursor.execute("UPDATE state SET symptom = %s WHERE caseID = %s ;", rr[rr.index('증상')+1], caseID)
     print("UPDATE 증상")
 
 if '산소포화도' in rr:
-    cursor.execute("UPDATE state SET oxygen = %s WHERE caseID = 'case00001' ;", rr[rr.index('산소포화도')+1])
+    cursor.execute("UPDATE state SET oxygen = %s WHERE caseID = %s ;", rr[rr.index('산소포화도')+1], caseID)
     print("UPDATE 산소포화도")
 
 
